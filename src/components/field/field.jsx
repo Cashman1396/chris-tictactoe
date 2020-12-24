a4723f759867d13eaaa15b209604629a1d8e50d0
Cashman1396 <Chrisashman96@gmail.com>
2020-12-24 12:24:31 -0500

    added playing field code

--- src/components/field/field.jsx
+++ src/components/field/field.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import './styles.css';
+
 class Field extends Component {
     render() {
         return (
-            <div>  
+            <div className="field" onClick={this.props.click}>
+                {this.props.children}
             </div>
         );
     }
