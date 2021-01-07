401f8701d1d68c49481ca16229557266ddcf7fca
Cashman1396 <Chrisashman96@gmail.com>
2021-01-07 10:21:41 -0500

    added code to game files

---
+++ src/pages/Game/game.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Swal from 'sweetalert2'
+import './game.css';
+import '../../assets/lib/bootstrap.min.css'
+
+class Game extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Game;
