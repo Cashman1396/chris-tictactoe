21697438bdb1a7f9d70e634b5719fbac2a3e47a5
Cashman1396 <Chrisashman96@gmail.com>
2021-01-09 14:57:16 -0500

    added round code to data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { Redirect, Link } from 'react-router-dom'
 
-import Board from '../../components/board';
-import Field from '../../components/field';
-import Button from '../../components/button';
+
+
 
 
 
